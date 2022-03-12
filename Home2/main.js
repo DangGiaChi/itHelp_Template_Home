e270c3540718d3d4676028a68d8394019eb725f4
DangGiaChi <danggiachi@gmail.com>
2022-03-13 00:13:00 +0700

    Still can't fix the parallax bug

--- Home2/main.js
+++ Home2/main.js
@@ -1,6 +1,11 @@
 const Hello = document.getElementById("parallax");
 
+// $(window).on('scroll', function() {
+//     window.requestAnimationFrame(scrollHandler);
+//  });
+
 window.addEventListener("scroll", function(){
     let offsetY = this.window.scrollY;
-    Hello.style.backgroundPositionY = (offsetY - Hello.offsetTop) * 0.7 + "px";
+    Hello.style.backgroundPositionY = (offsetY - Hello.offsetTop) * 0.7 + 'px';
 })
+
